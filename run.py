ac3ecebf76e214f9719fbefc62374b0c29c8a727
BaileyMuir <BaileyIRMuir@outlook.com>
2021-08-10 20:55:48 +0000

    Print the location of battleship to console to make sure it works and Add input method for users to pick row and column.

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ the grid is printed.
 """
 
 for x in range(0, 10):
-    board.append(["~"]*10)
+    board.append(["~"] * 10)
 
 
 for x in board:
@@ -31,18 +31,42 @@ for x in board:
 
 
 """
-the functions bellow should place a
+the functions below should place a
 random ship for the purpose of tesing
 the input method when it is added.
 """
 
 
 def random_row_coordinate(board):
-    return randint(0, len(board)-1)
+    return round(randint(0, len(board) -1))
 
 
 def random_column_coordinate(board):
-    return randint(0, len(board)-1)
+    return randint(0, len(board[0]) -1)
+
 
 random_row_coordinate(board)
 random_column_coordinate(board)
+
+"""
+Below are two variable both of which
+allow the user to make there guess 
+using input to avoid errors i will add
+custom error messages in future.
+"""
+row_guess = input("Guess the row:  \n")
+column_guess = input("Guess the column:  \n")
+
+"""
+Below is a variable holding the.
+random_row_coordinate data to display
+the location of the ship when printed.
+
+The same for the print 
+random_column_coordinate(board)
+as it will also show the location.
+"""
+random_ship = random_row_coordinate(board)
+
+print(random_ship)
+print(random_column_coordinate(board))
