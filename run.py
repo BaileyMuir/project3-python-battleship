6202c2b4d19682cdf265f25fa2799fc53f08a14b
BaileyMuir <BaileyIRMuir@outlook.com>
2021-08-10 14:36:21 +0000

    use PEP8 to find and fix errors

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
 # python code goes here
 
 """
-Created an empty variable for the board which the water and other 
-contents of the game will fix inside.
+Created an empty variable for the board
+which the water and other contents of
+the game will fix inside.
 """
 
 board = []
 
 """
-Here is a for loop that first goes through the process of producing a row of 10 inside of our
-board variable, inside which it will place "~" to represent the waves in the game and 
-produce 10 columns.
-After which it will goes through another loop to remove the [] present in the board variable from
-showing up when the grid is printed.
+Here is a for loop that first goes through
+the process of producing a row of 10 inside
+ of our board variable, inside which it will
+place "~" to represent the waves in the game
+and produce 10 columns.
+
+After which it will goes through another
+loop to remove the [] present in the
+board variable fromshowing up when
+the grid is printed.
 """
 
 for x in range(0, 10):
